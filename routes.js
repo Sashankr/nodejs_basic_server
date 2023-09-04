8b35fe3e98e3beb82758513cf4e4042b619a35d1
Sashankr <sashankr97@gmail.com>
2023-09-04 12:51:01 +0530

    Step 4: Display users created by admin in /users route

--- routes.js
+++ routes.js
@@ -17,7 +17,7 @@ const routesHandler = (req, res) => {
                 </style>
             </head>
             <body>
-                <h1>Admin Dashboard</h2>
+                <h1>Admin Dashboard</h1>
                 <p>Create a user by typing a name below and submitting the form.</p>
                 <form method="POST" action="/create-user">
                     <label for="username">Username</label>
@@ -38,9 +38,41 @@ const routesHandler = (req, res) => {
       const username = data.split("=")[1];
       usersList.push(username);
       console.log(usersList);
-      res.redirect("/users");
+      res.statusCode = 302;
+      res.setHeader("Location", "/users");
       return res.end();
     });
+  }
+  if (url === "/users") {
+    res.setHeader("Content-Type", "text/html");
+    const listOfUsers =
+      "<ul>" + usersList.map((item) => `<li>${item}</li>`).join("") + "</ul>";
+
+    res.write(`
+          <html>
+              <head>
+                  <title>Admin Dashboard</title>
+                  <style>
+                  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
+                    *{
+                      font-family : 'Poppins', sans-serif;
+                    }
+                    li{
+                      text-transform:capitalize;
+                    }
+                  </style>
+              </head>
+              <body>
+                  <h1>Users List</h1>
+                  <a href="/">Create New User</a>
+                  <p>List of users created by admin</p>
+                  <ol>
+                  ${listOfUsers}
+                  </ol>
+              </body>
+          </html>
+      `);
+    return res.end();
   }
 };
 
